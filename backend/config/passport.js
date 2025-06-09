9d53d55d35c0f10a5fcb783d15099a89db868d27
unknown <imadelhadjmoussa20@gmail.com>
2025-06-09 20:21:18 +0100

    updating the client url for backend

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -37,6 +37,11 @@ passport.use(new GoogleStrategy({
 
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
-    const user = await User.findById(id);
-    done(null, user);
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
+
