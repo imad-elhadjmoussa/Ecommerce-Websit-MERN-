10d6eb98778a2e1f56d2b085e973c50ecb7efbd3
unknown <imadelhadjmoussa20@gmail.com>
2025-06-09 19:34:39 +0100

    updating the client url for backend

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user.model');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback',
+    callbackURL: 'https://ecommerce-websit-mern-backend.onrender.com/api/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const email = profile.emails[0].value;
