b39c6489dc9f6a0f270602b4a8e25c5b548a3f21
unknown <imadelhadjmoussa20@gmail.com>
2025-06-11 18:50:12 +0100

    updating the server url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
 import path from "path"
+import { fileURLToPath } from 'url'
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,11 +17,15 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173, // or 5174 for the second project
-    historyApiFallback: true
+    port: 5173,
+    historyApiFallback: true,
+    strictPort: true,
+    host: true
   },
   preview: {
     port: 5173,
-    historyApiFallback: true
+    historyApiFallback: true,
+    strictPort: true,
+    host: true
   }
 })
